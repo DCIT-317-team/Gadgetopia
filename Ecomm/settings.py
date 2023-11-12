1da517146531517c917754bf8b8d1e6f80c78c1d
Alvin Bernasko <alvinbernasko123@gmail.com>
2023-11-12 23:14:08 +0000

    Add essential files

--- Ecomm/settings.py
+++ Ecomm/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5)&&#^wn*io_@+i1o*tav!0jj1$x^s^6p+k^biy%ymmud!_*q7'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- dockerfile
+++ dockerfile
@@ -1,7 +1,7 @@
 
 # The first instruction is what image we want to base our container on
 # We Use an official Python runtime as a parent image
-FROM python:3.11
+FROM --platform=amd64 python:3.11-alpine
 
 # The enviroment variable ensures that the python output is set straight
 # to the terminal with out buffering it first
